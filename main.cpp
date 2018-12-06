278bd1ec8d5f62a73d8fc0b6158e233a93e3b863
npd26 <37339554+npd26@users.noreply.github.com>
2018-12-06 23:43:28 +0100

    ch6-1,2
    ch6-1,2

--- main.cpp
+++ main.cpp
@@ -4,27 +4,23 @@ using namespace std;
 
 int main()
 {
-    int sum=0, i=1;
-    char first;
-    cout<<"the cureent sum is: "<<sum<<endl;
-    cout<<"do you want to add "<<i<<endl;
-    while(cin>>first)
+    int pin,pina;
+    int counter=5;
+    cout<<"enter pin: "<<endl;
+    cin>>pin;
+
+    do
+     {
+        cout<<"enter pin again: "<<endl;
+        cin>>pina;
+        counter--;
+     }
+    while((pina!=pin) && (counter>0));
+    if(pina==pin)
     {
-    if(first=='y' || first=='Y')
-    {
-        sum=sum+1;
-        cout<<"current sum is: "<<sum<<endl;
-        i++;
+       cout<<"correct pin";
     }
-    else if(first=='n' || first=='N')
-    {
-        cout<<"currnet sum is: "<<sum<<endl;
-        i++;
-    }
-    else{
-        break;
-    }
-    cout<<"do you want to add "<<i<<endl;
-    }
+    else{cout<<"5 time wrong pin entered";}
+
     return 0;
 }
